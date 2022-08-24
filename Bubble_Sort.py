300b0d892321dedd1cd99181c7b18ba5f2c4825f
Joseph Fong <111833444+Joseph-Fong@users.noreply.github.com>
2022-08-23 22:32:24 -0400

    Add files via upload

---
+++ Bubble_Sort.py
@@ -0,0 +1,13 @@
+list = []
+
+size = len(list)
+for s in range(size):
+    for v in range(size):
+        if v >= size-1:
+            break
+        else:
+            if list[v] > list[v+1]:
+                x = list[v]
+                list[v] = list[v+1]
+                list[v+1] = x
+print(list)
